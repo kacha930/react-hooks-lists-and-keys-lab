a4089204cfa74dae67b4e4657f428bed3ff9a93d
kacha930 <robiwere20@gmail.com>
2024-08-19 06:01:18 +0300

    corrected return nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return 
-   <nav>
+  return  <nav>
   <a href="#home">home</a>
   <a href="#about">about</a>
   <a href="#projects">projects</a>
